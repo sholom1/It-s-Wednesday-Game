5ba09b79ee3ba5ff4bebddde9a164808525861ad
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2020-04-22 21:14:44 -0400

    Added packages
    Added packages;
    Added testing scene;
    Added basic movement controls;
    Added some sounds;
    Frog Images.

---
+++ Wednesday/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public AudioSource ScreamingSource;
+    public AudioSource GunSource;
+    public AudioClip PewPew;
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal"), 0f) * speed);
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            ScreamingSource.UnPause();
+        else
+            ScreamingSource.Pause();
+        if (Input.GetKeyDown(KeyCode.F))
+            GunSource.PlayOneShot(PewPew);
+    }
+}
