178ff6a3142be6f1b8df5648c9f66a49f4b2f8af
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2020-04-22 22:40:27 -0400

    Added Coins

---
+++ Assets/Coin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public float RotateSpeed;
+    public float BobSpeed;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, (Mathf.Sin(Time.time) * Mathf.Deg2Rad) / 2);
+        transform.Rotate(0, RotateSpeed * Time.deltaTime, 0);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Coin Collected");
+    }
+}
